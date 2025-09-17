ea39925baabd517c5e8719daf3e2f2a153eeeff2
digambar342 <digambar.angul@gmail.com>
2025-09-17 23:47:30 +0530

    Remote web driver

--- src/test/java/driver/DriverRepo.java
+++ src/test/java/driver/DriverRepo.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,13 +16,15 @@ public class DriverRepo {
     private static ThreadLocal<WebDriver> tlDriver=new ThreadLocal<WebDriver>();
     private DriverRepo(){
     }
-    public static void setDriver(String browser){
+    public static void setDriver(String browser) throws MalformedURLException {
         if(browser.equalsIgnoreCase("chrome")){
             ChromeOptions options = new ChromeOptions();
             Map<String,Object> prefs= new HashMap<String,Object>();
             prefs.put("profile.password_manager_leak_detection", false);
             options.setExperimentalOption("prefs",prefs);
-            driver=new ChromeDriver(options);
+            options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+            //driver=new ChromeDriver(options);
+            driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
             tlDriver.set(driver);
         }else if(browser.equalsIgnoreCase("edge")){
             driver=new EdgeDriver();
